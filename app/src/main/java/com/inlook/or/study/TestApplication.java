6089933139fc2c077f89dba4fdd8beab6f17e0c9
ruishengli <li_inlook@126.com>
2016-07-01 16:43:42 +0800

    add dagger

--- app/src/main/java/com/inlook/or/study/TestApplication.java
+++ app/src/main/java/com/inlook/or/study/TestApplication.java
@@ -1,6 +1,13 @@
 package com.inlook.or.study;
 
 import android.app.Application;
+import com.inlook.or.study.dagger.AnalyticsManager;
+import com.inlook.or.study.dagger.AppComponent;
+import com.inlook.or.study.dagger.AppModule;
+import com.inlook.or.study.dagger.DaggerAppComponent;
+import com.inlook.or.study.dagger.DomainModule;
+
+import javax.inject.Inject;
 
 /**
  * desc TestApplication
@@ -10,10 +17,27 @@ import android.app.Application;
  */
 public class TestApplication extends Application {
     private static TestApplication mInstance;
+
+    AppComponent mAppComponent = null;
+    @Inject
+    AnalyticsManager analyticsManager;
+
     @Override
     public void onCreate() {
         super.onCreate();
         mInstance = this;
+        mAppComponent = DaggerAppComponent.builder()
+                .appModule(new AppModule(this))
+                .domainModule(new DomainModule())
+                .build();
+
+        mAppComponent.inject(this);
+        analyticsManager.register();
+    }
+
+
+    public AppComponent getAppComponent() {
+        return mAppComponent;
     }
 
     public static TestApplication getInstance() {
